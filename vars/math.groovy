c3fcd46ec5e92844cd94cd80daaebbc846820b4b
sai preetham <149465482+preetham-28@users.noreply.github.com>
2024-01-20 18:41:04 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,16 @@
 
+def sum(a,b)
+{
+  echo "the sum of ${a} and ${b} is ${a+b}"
+}
+
+
+def sub(a,b)
+{
+   echo "the subtraction of ${a} and ${b} is ${a-b}"
+}
+
+def multi(a,b)
+{
+   echo "the multiplication of ${a} and ${b} is ${a*b}"
+}
