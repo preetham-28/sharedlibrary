a767e7fc06db8b50ab163c5114fb37d416b6f679
sai preetham <149465482+preetham-28@users.noreply.github.com>
2024-01-26 11:42:29 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,5 +10,5 @@ def build()
 
 def tomcatdeploy(ip,contextpath)
 {
-     "deploy adapters: [tomcat9(credentialsId: '6568a0c6-613f-4317-84fb-685ed86709da', path: '', url: ${ip})], contextPath: ${contextpath}, war: '**/*.war"
+     "deploy adapters: [tomcat9(credentialsId: '6568a0c6-613f-4317-84fb-685ed86709da', path: '', url: '${ip}')], contextPath: '${contextpath}', war: '**/*.war"
 }
